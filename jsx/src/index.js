09de36cff9bca7d0d830e14e3420fa435da544f1
Arun BS <arunbs@Aruns-MBP.attlocal.net>
2019-06-16 12:53:52 +0530

    jsx changes

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import the React and ReactDOM libraries
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Create a react component
+function getButtonText() {
+  return "ClickMe!!!";
+}
+class Mycomp extends React.Component {
+  clickMe = () => {
+    return "You clicked on ME!!!";
+  };
+  render() {
+    const buttonName = "Click Me!!!";
+    return (
+      <div>
+        <label className="label" htmlFor="name">
+          Enter Name:
+        </label>
+        <input type="text" id="name" />
+        <button style={{ backgroundColor: "blue", color: "red" }}>
+          {buttonName}
+        </button>
+        <button style={{ backgroundColor: "blue", color: "red" }}>
+          {this.clickMe()}
+        </button>
+        <button style={{ backgroundColor: "blue", color: "red" }}>
+          {getButtonText()}
+        </button>
+      </div>
+    );
+  }
+}
+
+//Take the react component and show it on the screen
+ReactDOM.render(<Mycomp />, document.querySelector("#root"));
