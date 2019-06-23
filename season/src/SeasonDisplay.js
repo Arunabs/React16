e1920ab842819dd646abfbb55ee4440ebad79745
Arun BS <arunbs@Aruns-MBP.attlocal.net>
2019-06-23 11:05:04 +0530

    component code

--- season/src/SeasonDisplay.js
+++ season/src/SeasonDisplay.js
@@ -1,5 +1,36 @@
+import "./SeasonDisplay.css";
 import React from "react";
-
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const SeasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, its chilly",
+    iconName: "snowflake"
+  }
 };
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  var seasonName = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = SeasonConfig[seasonName];
+  return (
+    <div className={`season-display ${seasonName}`}>
+      <h1>Season Display</h1>
+      <br />
+      <i className={`icon-left ${iconName} icon massive`} />
+      <p className={`season-text`}>Season: {text}</p>
+      <i className={`icon-right ${iconName} icon massive`} />
+    </div>
+  );
+};
+
+export default SeasonDisplay;
--- season/src/index.js
+++ season/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errMessage: null };
+  }
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
@@ -14,15 +18,23 @@ class App extends React.Component {
       }
     );
   }
-  // Mandatory to declare the rendor
-  render() {
+  /**
+   * helper function to avoid the multiple return method inside the
+   * render method
+   */
+  getContent() {
     if (this.state.errMessage && !this.state.lat) {
       return <div> Error: {this.state.errMessage}</div>;
     }
     if (!this.state.errMessage && this.state.lat) {
-      return <div> Latitude : {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading!!!</div>;
+    return <Spinner />;
+  }
+
+  // Mandatory to declare the rendor
+  render() {
+    return <div className="border red">{this.getContent()}</div>;
   }
 }
 
